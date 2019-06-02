482326c6a0669deea6a940a2e4d171e0522be8f6
coderwhy <coderwhy@gmail.com>
2019-06-02 21:46:58 +0800

    小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,32 @@
-// pages/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    name: 'Coderwhy',
+    age: 18,
+    students: [
+      {id: 110, name: 'kobe', age: 30},
+      {id: 111, name: 'james', age: 28},
+      {id: 112, name: 'curry', age: 32},
+      {id: 113, name: 'why', age: 18}
+    ],
+    counter: 0
   },
+  handleBtnClick() {
+    // 1.错误做法: 界面是不会刷新的
+    // this.data.counter += 1
+    // console.log(this.data.counter)
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+    // 2.this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleSubtraction() {
+    this.setData({
+      counter: this.data.counter - 1
+    })
   }
 })
+
+// 编程范式: 
+// 1.命令式编程: 原生操作DOM
+// 2.声明式编程: Vue/React/Angular
